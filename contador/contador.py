22efac2ddc8b717964407f2bd172350750145d0a
Gabriel Bispo <gabrielsbisppo@gmail.com>
2026-06-15 00:26:06 -0300

    Adicionar limpar_texto()

--- contador/contador.py
+++ contador/contador.py
@@ -1,4 +1,12 @@
+def limpar_texto(texto):
+    texto = texto.lower()
+    carcteres = ",.%*!|?;:\"'()[]{}"
+    for char in carcteres:
+        texto = texto.replace(char, "")
+    return texto
+
 def contar_palavras (frase):
+    frase = limpar_texto(frase)
     palavras = frase.split()
     print(palavras)
     return len(palavras)
