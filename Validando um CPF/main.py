017c21eccc55cb9297d48c50c7a21bdbefd8e8aa
Gabriel Bispo <gabrielsbisppo@gmail.com>
2026-06-21 14:30:35 -0300

    Adicionar função para calcular o segundo dígito do CPF

--- Validando um CPF/main.py
+++ Validando um CPF/main.py
@@ -8,6 +8,17 @@ def primeiro_digito(cpf):
         return 0
     else:
         return 11 - resto
+    
+def segundo_digito(cpf):
+    fatores = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2]
+    # Separa os dígitos e multiplica cada um pelo seu fator correspondente
+    cpf_convertido = [int(digito) * fator for digito, fator in zip(cpf, fatores)]
+    soma = sum(cpf_convertido)
+    resto = soma % 11
+    if resto < 2:
+        return 0
+    else:
+        return 11 - resto
         
 
 def valida_cpf(cpf):
@@ -23,3 +34,4 @@ cpf = input('Digite seu CPF: ')
 
 print(valida_cpf(cpf))
 print(primeiro_digito(cpf))
+print(segundo_digito(cpf))
