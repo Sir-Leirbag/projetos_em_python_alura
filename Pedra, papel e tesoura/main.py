4f28c5ba1d64c876e30edca888c1f186b17d0189
Gabriel Bispo <gabrielsbisppo@gmail.com>
2026-06-27 20:44:11 -0300

    refatorar lógica do jogo para gerar jogada do computador após validação da entrada do usuário

--- Pedra, papel e tesoura/main.py
+++ Pedra, papel e tesoura/main.py
@@ -1,33 +1,26 @@
 import random
 
+opcoes = ['pedra', 'papel', 'tesoura']
+
 def gerar_elemento():
-    opcoes = ['pedra', 'papel', 'tesoura']
     return random.choice(opcoes)
 
 elemento = input('Escolha: pedra, papel ou tesoura? ')
-jogada_computador = gerar_elemento()
 jogada_usuario = elemento.strip().lower()
 
-if jogada_usuario == jogada_computador:
-    print(f'O computador escolheu: {jogada_computador}')
-    print('Empate!')
-elif jogada_usuario == 'pedra' and jogada_computador == 'tesoura':
-    print(f'O computador escolheu: {jogada_computador}')
-    print('Você venceu!')
-elif jogada_usuario == 'pedra' and jogada_computador == 'papel':
-    print(f'O computador escolheu: {jogada_computador}')
-    print('O computador venceu!')
-elif jogada_usuario == 'papel' and jogada_computador == 'pedra':
-    print(f'O computador escolheu: {jogada_computador}')
-    print('Você venceu!')
-elif jogada_usuario == 'papel' and jogada_computador == 'tesoura':
-    print(f'O computador escolheu: {jogada_computador}')
-    print('O computador venceu!')
-elif jogada_usuario == 'tesoura' and jogada_computador == 'papel':
-    print(f'O computador escolheu: {jogada_computador}')
-    print('Você venceu!')
-elif jogada_usuario == 'tesoura' and jogada_computador == 'pedra':
-    print(f'O computador escolheu: {jogada_computador}')
-    print('O computador venceu!')
-else:
+if jogada_usuario not in opcoes:
     print('Jogada inválida')
+else:
+    jogada_computador = gerar_elemento()
+    print(f'O computador escolheu: {jogada_computador}')
+
+    if jogada_usuario == jogada_computador:
+        print('Empate!')
+    elif (
+        jogada_usuario == 'pedra' and jogada_computador == 'tesora'
+        or jogada_usuario == 'papel' and jogada_computador == 'pedra'
+        or jogada_usuario == 'tesoura' and jogada_computador == 'papel'
+    ):
+        print('Você venceu!')
+    else:
+        print('O computador venceu!')
