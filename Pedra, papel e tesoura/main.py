3a0b97da657180ebec5fadd60b9d7eb5df1d85b4
Gabriel Bispo <gabrielsbisppo@gmail.com>
2026-06-26 22:21:05 -0300

    Adicionar implementação do jogo Pedra, Papel e Tesoura

---
+++ Pedra, papel e tesoura/main.py
@@ -0,0 +1,34 @@
+import random
+
+def gerar_elemento():
+    opcoes = ['Pedra', 'Papel', 'Tesoura']
+    return random.choice(opcoes)
+
+#def limpar_entrada(elemento):
+#    elemento = elemento.islower()
+
+elemento = input('Escolha: Pedra, Papel ou Tesoura? ')
+jogada_computador = gerar_elemento()
+
+if elemento == jogada_computador:
+    print(f'O computador escolheu: {jogada_computador}')
+    print('Empate!')
+elif elemento == 'Pedra' and jogada_computador == 'Tesoura':
+    print(f'O computador escolheu: {jogada_computador}')
+    print('Você venceu!')
+elif elemento == 'Pedra' and jogada_computador == 'Papel':
+    print(f'O computador escolheu: {jogada_computador}')
+    print('O computador venceu!')
+elif elemento == 'Papel' and jogada_computador == 'Pedra':
+    print(f'O computador escolheu: {jogada_computador}')
+    print('Você venceu!')
+elif elemento == 'Papel' and jogada_computador == 'Tesoura':
+    print(f'O computador escolheu: {jogada_computador}')
+    print('O computador venceu!')
+elif elemento == 'Tesoura' and jogada_computador == 'Papel':
+    print(f'O computador escolheu: {jogada_computador}')
+    print('Você venceu!')
+elif elemento == 'Tesoura' and jogada_computador == 'Pedra':
+    print(f'O computador escolheu: {jogada_computador}')
+    print('O computador venceu!')
+#print(gerar_elemento())
