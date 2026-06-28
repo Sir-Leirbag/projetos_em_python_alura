f8eba253e7955fd15c1e26a9e84e961fccd9f6a2
Gabriel Bispo <gabrielsbisppo@gmail.com>
2026-06-28 14:00:40 -0300

    formatar código e otimizar contagem de tentativas no jogo de adivinhação

--- Jogo de adivinhar o número/main.py
+++ Jogo de adivinhar o número/main.py
@@ -1,7 +1,7 @@
 import random
 
 def gerar_numero_aleatorio():
-    return random.randint(1,100)
+    return random.randint(1, 100)
 
 numero_aleatorio = gerar_numero_aleatorio()
 tentativas = 0
@@ -15,13 +15,11 @@ while True:
     if numero < 1 or numero > 100:
         print('Entrada inválida: Número fora do intervalo! Digite um número entre 1 e 100.')
         continue
-    elif numero > numero_aleatorio:
+    tentativas += 1
+    if numero > numero_aleatorio:
         print(f'Muito alto! Tente novamente: {numero}')
-        tentativas += 1
     elif numero < numero_aleatorio:
         print(f'Muito baixo! Tente novamente: {numero}')
-        tentativas += 1
     else:
-        tentativas += 1
         print(f'Parabéns! Você acertou o número: {numero} com {tentativas} tentativas.')
         break
